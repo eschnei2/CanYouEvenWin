909a91106b7e0557d19fcfdeba156f6762a55765
eschnei2 <everettschneider@gmail.com>
2021-10-29 09:54:19 -0500

    finished details

--- FirebaseMVC/Repositories/IContestRepository.cs
+++ FirebaseMVC/Repositories/IContestRepository.cs
@@ -8,6 +8,13 @@ namespace CanYouEvenWin.Repositories
 {
     public interface IContestRepository
     {
-      List<Contest> GetAllContests();
+       List<Contest> GetAllContests();
+       Contest GetContestById(int id);
+
+       public List<Prize> GetPrizeByContestId(int id);
+       public void DeleteContest(int Id);
+       public void AddContest(Contest contest);
+
+        public void UpdateContest(Contest contest);
     }
 }
